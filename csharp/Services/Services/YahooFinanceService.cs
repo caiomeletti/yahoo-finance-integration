4aba4df8d516d9cd33993e668aae0429989b80c8
Caio Meletti <meletti@gmail.com>
2024-09-18 11:04:00 -0300

    ⚡ perf: Threads paralelas na integração YF

--- csharp/Services/Services/YahooFinanceService.cs
+++ csharp/Services/Services/YahooFinanceService.cs
@@ -19,35 +19,39 @@ namespace Services.Services
 
         public async Task<bool> GetChartAndSave(List<string> tickers, string filename)
         {
-            // service atualiza valores da lista
-            // service salva o arquivo
-            var records = new List<ChartDTO>();
+            var tasks = new List<Task>();
+            var records = new Dictionary<string, ChartDTO>();
 
             foreach (var ticker in tickers)
             {
-                //todo mapear ticker (.sa e ibov)
-                var ret = await _yahooFinanceIntegration.GetChart(ticker,
-                    ValidRange.D1.ToString(), ValidRange.D1.ToString());
-
-                if (ret == null)
-                    continue;
-
-                records.Add(new ChartDTO
+                records.Add(ticker, new ChartDTO { Symbol = ticker });
+                Task t = Task.Run(async () =>
                 {
-                    Symbol = ret.Chart.Result.First().Meta.Symbol,
-                    ShortName = ret.Chart.Result.First().Meta.ShortName,
-                    RegularMarketTime = Util.UnixTimeStampToDateTime(ret.Chart.Result.First().Meta.RegularMarketTime),
-                    High = ret.Chart.Result.First().Indicators.Quote.First().High.First(),
-                    Low = ret.Chart.Result.First().Indicators.Quote.First().Low.First(),
-                    Close = ret.Chart.Result.First().Indicators.Quote.First().Close.First(),
-                    Open = ret.Chart.Result.First().Indicators.Quote.First().Open.First(),
-                    Volume = ret.Chart.Result.First().Indicators.Quote.First().Volume.First(),
-                    Adjclose = ret.Chart.Result.First().Indicators.Adjclose.First().AdjcloseValues.First(),
-                    Currency = ret.Chart.Result.First().Meta.Currency
-                });
-            }
+                    var ret = await _yahooFinanceIntegration.GetChart(ticker, ValidRange.D1.ToString(), ValidRange.D1.ToString());
 
-            return Util.CreateCsvFile(filename, records);
+                    if (ret != null)
+                    {
+                        //Debug.WriteLine($"{ticker} {ret.Chart.Result.First().Indicators.Adjclose.First().AdjcloseValues.First()}");
+                        records[ticker] = new ChartDTO
+                        {
+                            Symbol = ret.Chart.Result.First().Meta.Symbol,
+                            ShortName = ret.Chart.Result.First().Meta.ShortName,
+                            RegularMarketTime = Util.UnixTimeStampToDateTime(ret.Chart.Result.First().Meta.RegularMarketTime),
+                            High = ret.Chart.Result.First().Indicators.Quote.First().High.First(),
+                            Low = ret.Chart.Result.First().Indicators.Quote.First().Low.First(),
+                            Close = ret.Chart.Result.First().Indicators.Quote.First().Close.First(),
+                            Open = ret.Chart.Result.First().Indicators.Quote.First().Open.First(),
+                            Volume = ret.Chart.Result.First().Indicators.Quote.First().Volume.First(),
+                            Adjclose = ret.Chart.Result.First().Indicators.Adjclose.First().AdjcloseValues.First(),
+                            Currency = ret.Chart.Result.First().Meta.Currency
+                        };
+                    }
+                });
+                tasks.Add(t);
+            }
+            Task.WaitAll(tasks.ToArray(), -1);
+
+            return Util.CreateCsvFile(filename, records.Values.ToList());
         }
     }
 }
